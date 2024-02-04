f5ced6d44a59efddf01af132efe0a4db5efb8908
Mohib <mohibullahmohim2020@gmail.com>
2024-02-05 02:01:39 +0600

    Some work are done

--- app1.js
+++ app1.js
@@ -14,32 +14,60 @@ function fahrenheit_(temper){
     return celsius;
 }
 let celsius_ = fahrenheit_(Number(113));
-document.write( 'Celsius = ', celsius_);
+document.write( 'Celsius = ', celsius_ , '<br>');
 
 /*
 Task-2: 
 You are given an array of numbers. Count how many times the a number is repeated in the array. 
-
 sample-input: 
 numbers = [5,6,11,12,98, 5]
-
 find: 5
-
 output: 2
+ */
+let numbers = [5,6,11,12,98, 5]
 
-<br/>
 
+/**
 sample-input: 
-
 numbers = [5,6,11,12,98, 5]
-
 find: 25
-
 output: 0
+*/
 
+/*
 ### Task-3: 
 Write a function to count the number of vowels in a string.
+ */
+function text(text_string){
+    document.write( '<br>', text_string , '<br>');
+    text_array = text_string.split('');
+    document.write(text_array , '<br>') ;
+    let count = 0;
+    for (const letter of text_array) {
+        // document.write(letter , '<br>') ;
+        if(letter === 'a'){
+            count++;
+        }
+        else if(letter === 'e'){
+            count++;
+        }
+        else if(letter === 'i'){
+            count++;
+        }
+        else if(letter === 'o'){
+            count++;
+        }
+        else if(letter === 'u'){
+            count++;
+        }
+    }
+    return count;
+    // document.write( '<br>', count , ' vowels in this string. <br>');
+}
+let countVowels = text('a brown fox jump over the the lazy dog');
+document.write( '<br>', countVowels , ' vowels in this string. peo peo <br>');
 
+/**
 ### Task-4: 
 Write a function to find the longest word in a given string.
 
@@ -52,3 +80,8 @@ sample-output: Programming
 Generate a random number between 10 to 20.
 
  */
+let random = Math.random();
+let inTeger = (random * 10) ;
+let upToTen = inTeger +10 ;
+document.write('<br> My Random Number = ', upToTen , '<br>');
+
